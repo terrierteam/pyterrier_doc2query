e5ac607fa1563a3abcd2a744f0e19a8516a10b1e
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2023-07-20 21:16:35 +0100

    check that lfs is installed

--- pyterrier_doc2query/artefact.py
+++ pyterrier_doc2query/artefact.py
@@ -4,9 +4,23 @@ from hashlib import sha256
 from pathlib import Path
 import git
 
+_git_has_lfs_ = None
+def _git_has_lfs():
+    global _git_has_lfs_
+    if _git_has_lfs_ is not None:
+        return _git_has_lfs_
+    import git.cmd
+    try:
+        git.cmd.Git(".").execute(["git", "lfs", "env"])
+        _git_has_lfs_ = True
+    except git.exc.GitCommandError:
+        _git_has_lfs_ = False
+    return _git_has_lfs_
+
 class Artefact:
     @classmethod
     def from_repo(cls, repo_url, *args, **kwargs):
+        assert _git_has_lfs(), "git lfs support is required"
         repo_url_hash = sha256(repo_url.encode()).hexdigest()
         path = Path.home()/'.pyterrier'/'repos'/repo_url_hash
         meta_path = Path.home()/'.pyterrier'/'repos'/(repo_url_hash + '.json')
